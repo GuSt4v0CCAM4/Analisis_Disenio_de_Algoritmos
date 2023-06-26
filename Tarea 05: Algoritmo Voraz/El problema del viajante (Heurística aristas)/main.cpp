a3b1433196b8ed0fe4a78d670044dd6e71f1b7b6
GuSt4v0CCAM4 <alonsospinola70@gmail.com>
2023-06-25 21:04:54 -0500

    23/06/2023

--- Tarea 05: Algoritmo Voraz/El problema del viajante (Heurística aristas)/main.cpp
+++ Tarea 05: Algoritmo Voraz/El problema del viajante (Heurística aristas)/main.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 #include <list>
 #include <limits>
-
+using namespace std;
 class Graph {
     int numVertices;
-    std::list<std::pair<int, int>>* adjacencyList;
+    list<pair<int, int>>* adjacencyList;
 
 public:
     Graph(int vertices) : numVertices(vertices) {
-        adjacencyList = new std::list<std::pair<int, int>>[numVertices];
+        adjacencyList = new list<pair<int, int>>[numVertices];
     }
 
     void addEdge(int source, int destination, int weight) {
-        adjacencyList[source].push_back(std::make_pair(destination, weight));
-        adjacencyList[destination].push_back(std::make_pair(source, weight));
+        adjacencyList[source].push_back(make_pair(destination, weight));
+        adjacencyList[destination].push_back(make_pair(source, weight));
     }
 
     void tsp(int& totalCost) {
@@ -23,13 +23,13 @@ public:
         }
         visited[0] = true;  // Empezar en el nodo 0
 
-        std::list<std::pair<int, int>> path;
+        list<pair<int, int>> path;
         int currentVertex = 0;
         int minCost = 0;
 
         for (int count = 0; count < numVertices - 1; ++count) {
             int nextVertex = -1;
-            int minWeight = std::numeric_limits<int>::max();
+            int minWeight = numeric_limits<int>::max();
 
             for (const auto& neighbor : adjacencyList[currentVertex]) {
                 int vertex = neighbor.first;
@@ -43,24 +43,24 @@ public:
 
             if (nextVertex != -1) {
                 visited[nextVertex] = true;
-                path.push_back(std::make_pair(currentVertex, nextVertex));
+                path.push_back(make_pair(currentVertex, nextVertex));
                 minCost += minWeight;
                 currentVertex = nextVertex;
             }
         }
 
-        path.push_back(std::make_pair(currentVertex, 0));
+        path.push_back(make_pair(currentVertex, 0));
         minCost += adjacencyList[currentVertex].front().second;
 
         totalCost = minCost;
 
-        std::cout << "Camino óptimo por aristas: ";
+        cout << "Camino óptimo por aristas: ";
         for (const auto& edge : path) {
-            std::cout << "(" << edge.first << "," << edge.second << ") ";
+            cout << "(" << edge.first << "," << edge.second << ") ";
         }
-        std::cout << std::endl;
+        cout << endl;
 
-        std::cout << "Costo total del camino: " << totalCost << std::endl;
+        cout << "Costo total del camino: " << totalCost << endl;
 
         delete[] visited;
     }
